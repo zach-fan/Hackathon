a0c27860a80c0efb4e5998ed952ed69a786addb4
zach-fan <zach-fan@github.com>
2020-07-21 12:01:02 -0400

    game

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var dungeon = ["Coin", "Coin", "Soup", "Meat", "Potato", "Goblin", "Skeleton", "Witch", "Snake", "Nothing"]
+var dungeon = ["Coin", "Coin", "Soup", "Meat", "Potato", "Goblin", "Skeleton", "Orc", "Snake", "Nothing"]
 var coins = 0
 var health = 3
 var food = 5
@@ -6,30 +6,127 @@ var food = 5
 
 
 
-for (var m= 0; m < 10; m++){
+for (var m = 0; m < 10; m++) {
     document.write()
     console.log("crossroad" + m)
 
-    document.onkeyup = function (event){
+    document.onkeyup = function (event) {
         var keypressed = event.key;
-        if(keypressed = "w"){
+        if (keypressed = "w") {
             document.write("Forward text")
-        }else if(keypressed = "a"){
-            document. write("Left text")
-        }else if(keypressed = "d"){
+        } else if (keypressed = "a") {
+            document.write("Left text")
+        } else if (keypressed = "d") {
             document.write("Right text")
-        }else{
+        } else {
 
         }
-        document.onkeyup = function(event) {
-        if (event.key === "w" || event.key === "a" || event.key === "d")
-{
-    var encounter = dungeon [Math.floor(Math.random() * dungeon.length)];
+        document.onkeyup = function (event) {
+            if (event.key === "w" || event.key === "a" || event.key === "d") {
+                food--
+                var encounter = dungeon[Math.floor(Math.random() * dungeon.length)];
 
-    console.log(encounter)
+                console.log(encounter);
+                console.log("Food: " + food);
 
-}
+                if (encounter = "Coin") {
+                    coins++
+                    console.log("Coins: " + coins)
+                };
+
+                if (encounter === "Soup" || encounter === "Meat" || encounter === "Potato") {
+
+                    food++
+                };
+
+                if (encounter === "Goblin") {
+                    console.log("Oh no a Golbin, use either punch, kick, or block to beat it! Make sure you use the correct one!")
+                    document.onkeyup = function (event) {
+                        if (event.key = "b") {
+                            console.log("Congrats you beat the Goblin")
+
+
+                        }
+                        else {
+                            console.log("You Lost 1 Heart")
+                            health--
+                            document.write("Health: " + health)
+                            m--
+
+
+
+                        }
+                    }
+                };
+
+                if (encounter === "Skeleton") {
+                    console.log("Oh no a Skeleton, use either punch, kick, or block to beat it! Make sure you use the correct one!")
+                    document.onkeyup = function (event) {
+                        if (event.key = "p") {
+                            console.log("Congrats you beat the Goblin")
+
+
+                        }
+                        else {
+                            console.log("You Lost 1 Heart")
+                            health--
+                            document.write("Health: " + health)
+                            m--
+
+
+
+                        }
+                    }
+                };
+
+                if (encounter === "Orc") {
+                    console.log("Oh no a Orc, use either punch, kick, or block to beat it! Make sure you use the correct one!")
+                    document.onkeyup = function (event) {
+                        if (event.key = "k") {
+                            console.log("Congrats you beat the Goblin")
+
+
+                        }
+                        else {
+                            console.log("You Lost 1 Heart")
+                            health--
+                            document.write("Health: " + health)
+                            m--
+
+
+
+                        }
+                    }
+                }
+
+                if (encounter === "Snake") {
+                    console.log("Oh no a Snake, use either punch, kick, or block to beat it! Make sure you use the correct one!")
+                    document.onkeyup = function (event) {
+                        if (event.key = "p") {
+                            console.log("Congrats you beat the Goblin")
+                            document.write("Health: " + health)
+
+
+
+                        }
+                        else {
+                            console.log("You Lost 1 Heart")
+                            health--
+                            document.write("Health: " + health)
+                            m--
+
+
+
+                        }
+                    }
+                }
+
+
+
+
+
+            }
+        }
     }
-}
 
 }

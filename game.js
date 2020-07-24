592cb7dc555d94d301e228d57a6e029683b81715
zach-fan <zach-fan@github.com>
2020-07-24 10:45:31 -0400

    BETTER

--- game.js
+++ game.js
@@ -45,173 +45,193 @@ addEventListener("keydown", function (e) {
 
 
 function startGame() {
-    if (health <= 0 || food <= 0) {
+    if (health < 0 || food < 0) {
         alert("You Lose, Click End Game Button To Return Home")
         window.location.replace("index.html")
+    }
+
+
+if (m >= 10) {
+    alert("You're Going to Boss Room")
+    window.location.replace("Bossfight.html")
+
+
+    //Boss ROOM GO
+}
+
+
+document.onkeyup = function (event) {
+    if (event.key === "w" || event.key === "a" || event.key === "d") {
+
+
+        var encounter = dungeon[Math.floor(Math.random() * dungeon.length)];
+
+        console.log(encounter);
+        console.log("Food: " + food);
+        var healthDiv = document.getElementById("health-text");
+        healthDiv.textContent = "Health: " + health;
+        var foodDiv = document.getElementById("food-text");
+        foodDiv.textContent = "Food: " + food;
+        var coinDiv = document.getElementById("coin-text");
+        coinDiv.textContent = "Coins: " + coins;
+        var encounterDiv = document.getElementById("encounter-text");
+        encounterDiv.textContent = "You Encounter A " + encounter;
+        
+        
+        
+        
+
+        if (encounter === "Coin") {
+            coins++
+            m++
+            console.log("Coins: " + coins)
+            startGame()
+
+        };
+        if (encounter === "Nothing") {
+            m++
+            startGame()
+        }
+
+        if (encounter === "Soup" || encounter === "Meat" || encounter === "Potato") {
+
+            food++
+            food++
+            m++
+            startGame()
+        };
+
+
+        if (encounter === "Goblin") {
+
+           var monsterDiv = document.getElementById("monster-encounter");
+        monsterDiv.textContent = "As you pass through the dungeon, you smell an awful stench. You slink closer only to find a solitary goblin finishing up a drum stick. when he sees you throws it aside and prepares to make your bones into plaster."
+            document.onkeyup = function (event) {
+                var keyCode = (event).keyCode
+
+                if (keyCode === 66) {
+                    monsterDiv.textContent = "You defeat the foul goblin and continue on your way. Good Job!"
+                    m++
+                    startGame()
+
+
+
+
+
+                }
+
+
+                else {
+                    console.log("After get absolutely trounced by the goblin you barely manage to take it down. You have lost one life.")
+                    var loseDiv = document.getElementById("lose-heart");
+            loseDiv.textContent = "You Lost 1 Heart"
+                    health--
+
+                    m++
+                    startGame()
+                }
+            }
+
+
+
+
+
+        }
+
+
+
+        if (encounter === "Skeleton") {
+            var monsterDiv = document.getElementById("monster-encounter");
+        monsterDiv.textContent = "You run into a room that looks like the kind of crypt that you imagine being in the basement of a chapel. That is, not including the skeleton shambling over trying to kill you."
+            document.onkeyup = function (event) {
+                var keyCode = (event).keyCode
+
+                if (keyCode === 80) {
+                    monsterDiv.textContent = "you breakk apart its bones and then stomp them into dust."
+                    m++
+
+                    startGame()
+
+
+                }
+                else {
+                    var loseDiv = document.getElementById("lose-heart");
+            loseDiv.textContent = "You Lost 1 Heart"
+                    health--
+                    monsterDiv.textContent = "The skeleton knocks you into a wall and then cracks your skull, but before it can finish you off, the bones turn to dust because they were so brittle."
+                    m++
+                    startGame()
+
+
+                }
+            }
+        }
+
+
+
+        if (encounter === "Orc") {
+            var monsterDiv = document.getElementById("monster-encounter");
+            monsterDiv.textContent = "An earth-shattering roar splits your eardrums as an orc barricades your path. You sigh and prepare for a battle."
+           
+            document.onkeyup = function (event) {
+                var keyCode = (event).keyCode
+                if (keyCode === 75) {
+                    monsterDiv.textContent =  "You best the orc and after searching their pockets like a true adventurer, you move on to the next room"
+                    m++
+                    startGame()
+                }
+                else {
+                    monsterDiv.textContent = "After being battered by the Orc, it slips and falls. allowing you to whail on it. You lost one life."
+                    var loseDiv = document.getElementById("lose-heart");
+            loseDiv.textContent = "You Lost 1 Heart"
+                    health--
+
+                    m++
+                    startGame()
+
+
+                }
+            }
+        }
+
+
+        if (encounter === "Snake") {
+            var monsterDiv = document.getElementById("monster-encounter");
+            monsterDiv.textContent ="You are crossing into the next when you hear a rattle. You try to run but the snake that was in the room follows you and prepares to pump your blood so full of venom that it will be classified as a biohazard postmortem."
+        
+            document.onkeyup = function (event) {
+                var keyCode = (event).keyCode
+
+                if (keyCode === 80) {
+                    monsterDiv.textContent ="After wrestling the snake and beating it to a pulp, you continue on your journey."
+                   
+
+                    m++
+                    startGame()
+
+
+
+
+
+                }
+                else {
+                    monsterDiv.textContent = "You are able to take the life of the snake, but the venom from it's bite trickles into your bloodstream"
+
+                    var loseDiv = document.getElementById("lose-heart");
+            loseDiv.textContent = "You Lost 1 Heart"
+                    health--
+
+
+                    m++
+                }
+                    startGame()
+
+                }
+            }
         }
 
     }
-    if (m >= 10){
-        alert("You're Going to Boss Room")
-        window.location.replace("Bossfight.html")
-
-
-//Boss ROOM GO
-    }
-    
-
-    document.onkeyup = function (event) {
-        if (event.key === "w" || event.key === "a" || event.key === "d") {
-
-
-            var encounter = dungeon[Math.floor(Math.random() * dungeon.length)];
-
-            console.log(encounter);
-            console.log("Food: " + food);
-            var healthDiv = document.getElementById("health-text");
-    healthDiv.textContent = "Health: " + health;
-    var foodDiv = document.getElementById("food-text");
-    healthDiv.textContent = "Food: " + food;
-
-            if (encounter === "Coin") {
-                coins++
-                m++
-                console.log("Coins: " + coins)
-                startGame()
-
-            };
-            if (encounter === "Nothing"){
-                m++
-                startGame()
-            }
-
-            if (encounter === "Soup" || encounter === "Meat" || encounter === "Potato") {
-
-                food++
-                food++
-                m++
-                startGame()
-            };
-
-
-            if (encounter === "Goblin") {
-                console.log(" As you pass through the dungeon, you smell an awful stench. You slink closer only to find a solitary goblin finishing up a drum stick. when he sees you throws it aside and prepares to make your bones into plaster.")
-                document.onkeyup = function (event) {
-                    var keyCode = (event).keyCode
-
-                    if (keyCode === 66) {
-                        console.log("You defeat the foul goblin and continue on your way. Good Job!")
-                        m++
-                        startGame()
-
-
-
-
-
-                    }
-
-
-                    else {
-                        console.log("After get absolutely trounced by the goblin you barely manage to take it down. You have lost one life.")
-                        console.log("You Lost 1 Heart")
-                        health--
-                        
-                        m++
-                        startGame()
-                    }
-                }
-
-
-
-
-
-            }
-
-
-
-            if (encounter === "Skeleton") {
-                console.log("Oh no a Skeleton, use either punch, kick, or block to beat it! Make sure you use the correct one!")
-                document.onkeyup = function (event) {
-                    var keyCode = (event).keyCode
-
-                    if (keyCode === 80) {
-                        console.log("you breakk apart its bones and then stomp them into dust.")
-                        m++
-
-                        startGame()
-
-
-                    }
-                    else {
-                        console.log("You Lost 1 Heart")
-                        health--
-                       
-                        m++
-                        startGame()
-
-
-                    }
-                }
-            }
-
-
-
-            if (encounter === "Orc") {
-                console.log("Oh no a Orc, use either punch, kick, or block to beat it! Make sure you use the correct one!")
-                document.onkeyup = function (event) {
-                    var keyCode = (event).keyCode
-                    if (keyCode === 75) {
-                        console.log("You best the orc and after searching their pockets like a true adventurer, you move on to the next room")
-                        m++
-                        startGame()
-                    }
-                    else {
-                        console.log("After being battered by the Orc, it slips and falls. allowing you to whail on it. You lost one life.")
-                        console.log("You Lost 1 Heart")
-                        health--
-                       
-                        m++
-                        startGame()
-
-
-                    }
-                }
-            }
-
-
-            if (encounter === "Snake") {
-                console.log("Oh no a Snake, use either punch, kick, or block to beat it! Make sure you use the correct one!")
-                document.onkeyup = function (event) {
-                    var keyCode = (event).keyCode
-
-                    if (keyCode === 80) {
-                        console.log("After wrestling the snake and beating it to a pulp, you continue on your journey."
-                        )
-
-                        m++
-                        startGame()
-
-
-
-
-
-                    }
-                    else {
-                        console.log("You are able to take the life of the snake, but the venom from it's bite trickles into your bloodstream and makes you lose one life.")
-
-                        console.log("You Lost 1 Heart")
-                        health--
-                      
-
-                        m++
-                        startGame()
-
-                    }
-                }
-            }
-
-        }
-    }
+}
 
 
 
@@ -225,54 +245,3 @@ function startGame() {
 
 
 startGame()
-//boss fight
-var kpb = ["k", "p", "b"];
-var compWin = 0;
-var userWin = 0;
-document.write("As you approach the final room, your nose is assailed by the stench exuding from the bodies of the past adventurers. You steeel yourself for the battle to come. You enter into what looks like an arena, and opposite you is a hulking slab of meat that ould only be the minotaur. You know that it is your duty and your honor to face this monstrosity, to avenge your fallen comrades and to complete this game. Realizing that this is it, you brace yourself as the Minotaur charges.")
-    
-document.onkeyup = function(event){
-    var userChoice = event.key;
-            var computerChoice = kpb[Math.floor(Math.random() * kpb.length)];
-            console.log(computerChoice);
-        if (kpb.includes(userChoice)) {
-            if(userChoice === computerChoice){
-                console.log("tie" + userChoice);
-                document.write("You and the Minotaur chose the same move. Its a tie!");
-            } else if(userChoice === "p" && computerChoice === "k" || userChoice === "b" && computerChoice === "p" || userChoice === "k" && computerChoice === "b"){
-                document.write ("Thats one win for the Minotaur")
-                compWin++
-                console.log("lose" + userChoice);
-            }else{
-                document.write ("You won this round!")
-                userWin++
-                console.log ("win" + userChoice);
-            }
-            if(compWin < userWin){
-                var escape = true;
-                var fail = false;
-               alert(" Congratulations You Beat the Minotaur! Click on the button to exit the Maze");
-                if (escape === true){
-                    document.getElementById("end_button").style.display="block";
-                    document.getElementById("try_again").style.display="none";
-                } 
-                
-            }else{
-                alert("Unfortunately you did not beat the Minotaur and died. Click the button to try again");
-                fail = true;
-                escape = false;
-                if(fail === true){
-                    document.getElementById("try_again").style.display="block";
-                    document.getElementById("end_button").style.display="none";
-                } 
-                
-            }
-            function end() {
-                location.href = "end.html"
-              }
-              function restart() {
-                location.href = "game.html";
-              }
-          }
-        }
-
